5e47543debde6645e0f2b0125edb74bebd16f72b
James Orson <jamesaorson@gmail.com>
2022-09-21 16:29:17 -0700

    Add reference

---
+++ humidity/main.c
@@ -0,0 +1,10 @@
+// Video: https://www.youtube.com/watch?v=mdXPfwmuADk
+// Repo: https://github.com/1nformatica/WeMos-SHT-30-Temperature-Humidity-Sensor-
+
+void setup() {
+
+}
+
+void loop() {
+
+}
